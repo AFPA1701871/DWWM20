71b2c77c42f0c1c6350c0939b0222f1562451e6d
afpa1701871 <martine.poix@afpa.fr>
2020-11-18 16:07:53 +0100

    methode

--- 03 - Web statique/Exemples/Ex14 Les formulaires/formulaire.php
+++ 03 - Web statique/Exemples/Ex14 Les formulaires/formulaire.php
@@ -1,6 +1,7 @@
-
-
-<form method ="get" action = "resultat.php">
-<input name="nom"/>
-<button type="submit" >Envoyer</button>
+<form method="get" action="resultat.php">
+    <label for="nom">Nom : </label>
+    <input name="nom" placeholder="Saisir votre nom" type="text"/>
+    <input name="prenom" placeholder="Saisir votre prenom" type="text"/>
+    <input name="age" placeholder="Saisir votre age" type="text"/>
+    <button type="submit">Envoyer</button>
 </form>
