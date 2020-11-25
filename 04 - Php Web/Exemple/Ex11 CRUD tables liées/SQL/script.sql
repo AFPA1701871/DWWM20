6f93ebdd8014ce7e1419f2d43c00318ce36f23c3
afpa1701871 <martine.poix@afpa.fr>
2020-11-25 09:57:21 +0100

    bdd

--- 04 - Php Web/Exemple/Ex11 CRUD tables liées/SQL/script.sql
+++ 04 - Php Web/Exemple/Ex11 CRUD tables liées/SQL/script.sql
@@ -1,42 +1,29 @@
-CREATE DATABASE IF NOT EXISTS `baseproduits` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `baseproduits`;
+CREATE DATABASE IF NOT EXISTS baseproduits DEFAULT CHARACTER
+SET latin1 COLLATE latin1_swedish_ci;
 
--- --------------------------------------------------------
+USE baseproduits;
 
---
--- Structure de la table `categories`
---
+DROP TABLE IF EXISTS categories;
 
-DROP TABLE IF EXISTS `categories`;
-CREATE TABLE IF NOT EXISTS `categories` (
-  `idCategorie` int(11) NOT NULL AUTO_INCREMENT,
-  `LibelleCategorie` varchar(50) NOT NULL,
-  PRIMARY KEY (`idCategorie`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS categories (
+  idCategorie int (11) NOT NULL AUTO_INCREMENT,
+  LibelleCategorie varchar (50) NOT NULL,
+  PRIMARY KEY (idCategorie)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS produits;
 
---
--- Structure de la table `produits`
---
+CREATE TABLE IF NOT EXISTS produits (
+  idProduit int (11) NOT NULL AUTO_INCREMENT,
+  libelleProduit varchar (50) NOT NULL,
+  prix int (11) NOT NULL,
+  dateDePeremption date NOT NULL,
+  idCategorie int (11) NOT NULL,
+  PRIMARY KEY (idProduit),
+  KEY FK_Produit_Categorie (idCategorie)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
-DROP TABLE IF EXISTS `produits`;
-CREATE TABLE IF NOT EXISTS `produits` (
-  `idProduit` int(11) NOT NULL AUTO_INCREMENT,
-  `libelleProduit` varchar(50) NOT NULL,
-  `prix` int(11) NOT NULL,
-  `dateDePeremption` date NOT NULL,
-  `idCategorie` int(11) NOT NULL,
-  PRIMARY KEY (`idProduit`),
-  KEY `FK_Produit_Categorie` (`idCategorie`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `produits`
---
-ALTER TABLE `produits`
-  ADD CONSTRAINT `FK_Produit_Categorie` FOREIGN KEY (`idCategorie`) REFERENCES `categories` (`idCategorie`);
+ALTER TABLE
+  produits
+ADD
+  CONSTRAINT FK_Produit_Categorie FOREIGN KEY (idCategorie) REFERENCES categories (idCategorie);
