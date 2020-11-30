7a584c46b312f6d3936110dcf9804e10af4944b6
afpa1701871 <martine.poix@afpa.fr>
2020-11-30 09:54:28 +0100

    userConnect

---
+++ 04 - Php Web/Exemple/UserConnect/PHP/VIEW/FormConnection.php
@@ -0,0 +1,12 @@
+<form action="index.php?codePage=actionConnection" method="post">
+    <div>
+        <label for="pseudo">Pseudo</label>
+        <input type="text" name="pseudo" required />
+    </div>
+    <div>
+        <label for="motDePasse">mot De Passe</label>
+        <input type="password" name="motDePasse" required />
+    </div>
+    
+    <button type="submit">Valider</button>
+</form>
