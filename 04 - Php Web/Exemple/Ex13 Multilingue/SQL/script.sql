53d9ec46d80272299295a7fd71c9d1a27faa7c2c
afpa1701871 <martine.poix@afpa.fr>
2020-12-01 09:46:39 +0100

    multilingue

--- 04 - Php Web/Exemple/Ex13 Multilingue/SQL/script.sql
+++ 04 - Php Web/Exemple/Ex13 Multilingue/SQL/script.sql
@@ -1,21 +1,31 @@
 
-CREATE DATABASE IF NOT EXISTS `userconnect` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `userconnect`;
+CREATE DATABASE IF NOT EXISTS `multilingue` ;
+USE `multilingue`;
 
-DROP TABLE IF EXISTS `utilisateurs`;
-CREATE TABLE IF NOT EXISTS `utilisateurs` (
-  `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(50) NOT NULL,
-  `prenom` varchar(50) NOT NULL,
-  `motDePasse` varchar(50) NOT NULL,
-  `adresseMail` varchar(50) NOT NULL,
-  `role` int(11) NOT NULL COMMENT '1 Admin 2 User',
-  `pseudo` varchar(50) NOT NULL,
-  PRIMARY KEY (`idUtilisateur`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `texte`;
+CREATE TABLE IF NOT EXISTS `texte` (
+  `idTexte` int(11) NOT NULL AUTO_INCREMENT,
+  `codeTexte` varchar(20) NOT NULL,
+  `codeLangue` varchar(2) NOT NULL,
+  `Texte` varchar(200) NOT NULL,
+  PRIMARY KEY (`idTexte`),
+  KEY `Code Texte` (`codeTexte`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `texte`
+--
 
-INSERT INTO `utilisateurs` (`idUtilisateur`, `nom`, `prenom`, `motDePasse`, `adresseMail`, `role`, `pseudo`) VALUES
-(2, 'sd', 'sd', '6226f7cbe59e99a90b5cef6f94f966fd', 'sd', 1, 'sd'),
-(5, 'qs', 'qs', '304854e2e79de0f96dc5477fef38a18f', 'qs', 1, 'qs'),
-(6, 'tr', 'rt', '822050d9ae3c47f54bee71b85fce1487', 'rt', 1, 'rt');
+INSERT INTO `texte` (`idTexte`, `codeTexte`, `codeLangue`, `Texte`) VALUES
+(null, 'Accueil', 'FR', 'Accueil'),
+(null, 'titrePage', 'FR', 'Gestion des articles'),
+(null, 'accueil', 'EN', 'Home'),
+(null, 'titrePage', 'EN', 'Article Management'),
+(null, 'Afficher', 'EN', 'Display'),
+(null, 'Modifier', 'EN', 'Update'),
+(null, 'Supprimer', 'EN', 'Remove'),
+(null, 'Afficher', 'FR', 'Afficher'),
+(null, 'Modifier', 'FR', 'Modifier'),
+(null, 'Supprimer', 'FR', 'Supprimer'),
+(null, 'art1', 'FR', 'c\'est un article '),
+(null, 'art1', 'EN', 'this is an article');
