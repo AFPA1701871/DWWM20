10d3e543e31a5977f4d45eafa569be7e99c1aa39
afpa1701871 <martine.poix@afpa.fr>
2020-12-14 08:23:21 +0100

    ex js

---
+++ 06 - Web Dynamique/01 - Javascript/Exemples/Ex2/script.js
@@ -0,0 +1,5 @@
+var tab=['a','b','c','a'];
+var tab2 ='toto';
+resultat=tab2.indexOf('o');
+console.log(resultat);
+document.write('<img src="logo_Afpa.jpg"></img>');
---
+++ 06 - Web Dynamique/01 - Javascript/Exemples/Ex3 Les fonctions/script.js
@@ -0,0 +1,30 @@
+var rep = parseInt(prompt("1- Multiples\n2- Somme et moyenne\n\nEntrez votre option :"));
+
+switch (rep) {
+    case 1:
+        var nbr = parseInt(prompt("Entrez un nombre à multiplier"))
+        function TableMultiplication(nbr) {
+            for (let i = 0; i <= 10; i++) {
+                let ligne = i + " x " + nbr + " = " + (i * nbr) + "\n";
+                document.write(ligne);
+            }
+        }
+        TableMultiplication(nbr);
+        break;
+    case 2:
+        function moyenne() {
+            let somme = 0;
+            let cpt = 0;
+            do {
+                var nbr = parseInt(prompt("Entrez un nombre(zéro pour arrêter le programme)"));
+                if (nbr != 0) {
+                    somme = somme + nbr;
+                    cpt++;
+                }
+            } while (nbr != 0)
+            document.write("La somme des nombres est de " + somme + "\nLa moyenne est de " + (somme / cpt))
+        }
+        moyenne();
+        break;
+    }
+    
