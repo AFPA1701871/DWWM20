a3337b1c8b59aa388176491e20d3b12e681336fd
afpa1701871 <martine.poix@afpa.fr>
2021-01-15 08:44:47 +0100

    head

--- Projet/Conventions/PHP/VIEW/Head.php
+++ Projet/Conventions/PHP/VIEW/Head.php
@@ -1,8 +1,9 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
 <head>
-	<meta charset="utf-8">
-	<title><?php echo $titre ?></title>
-	<link rel="stylesheet" href="./CSS/style.css">
-	<script src="./JS/script.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/style.css">
+    <title>Titre</title>
 </head>
+<body>
