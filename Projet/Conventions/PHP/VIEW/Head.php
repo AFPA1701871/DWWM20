1101d299fdb10224591dee83be22a8f5b627e1cb
afpa1701871 <martine.poix@afpa.fr>
2021-01-15 08:46:59 +0100

    css

--- Projet/Conventions/PHP/VIEW/Head.php
+++ Projet/Conventions/PHP/VIEW/Head.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/style.css">
-    <title>Titre</title>
+    <link rel="stylesheet" href="../CSS/Root.css">
+    <link rel="stylesheet" href="../CSS/Style.css">
+    <title>Gestion des conventions</title>
 </head>
 <body>
