e64f74f0e70fc52fdabf979b7e3c0e0ea216b6d3
AFPA1701871 <martine.poix@afpa.fr>
2021-02-11 17:39:33 +0100

    eval

--- 98 - Evaluation/07 - CMS/horaire/horaireClass.php
+++ 98 - Evaluation/07 - CMS/horaire/horaireClass.php
@@ -10,10 +10,16 @@ class horaireClass
     }
 
     // Installation initiale dans la BDD
-    public static function install()
-    {
+    public static function install(){
         global $wpdb;
         $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}horaire (id INT AUTO_INCREMENT PRIMARY KEY, jour VARCHAR(30) NOT NULL, horaire_matin VARCHAR(50), horaire_apresMidi VARCHAR(50), premierJour INT DEFAULT 0);");
+        $wpdb->insert("{$wpdb->prefix}horaire",array("jour"=>"Lundi","horaire_matin"=>"8h30-12h30","horaire_apresMidi"=>"13h30-17h30"));
+		$wpdb->insert("{$wpdb->prefix}horaire",array("jour"=>"Mardi","horaire_matin"=>"8h30-12h30","horaire_apresMidi"=>""));
+		$wpdb->insert("{$wpdb->prefix}horaire",array("jour"=>"Mecredi","horaire_matin"=>"8h30-12h30","horaire_apresMidi"=>"13h30-17h30"));
+		$wpdb->insert("{$wpdb->prefix}horaire",array("jour"=>"Jeudi","horaire_matin"=>"Fermeture","horaire_apresMidi"=>""));
+		$wpdb->insert("{$wpdb->prefix}horaire",array("jour"=>"Vendredi","horaire_matin"=>"8h30-12h30","horaire_apresMidi"=>"13h30-17h30"));
+		$wpdb->insert("{$wpdb->prefix}horaire",array("jour"=>"Samedi","horaire_matin"=>"","horaire_apresMidi"=>"13h30-17h30"));
+		$wpdb->insert("{$wpdb->prefix}horaire",array("jour"=>"Dimanche","horaire_matin"=>"8h30-12h30","horaire_apresMidi"=>""));
     }
 
     // Désinstallation dans la BDD
